b53ca4be244992da4db372320010e260c7da8704
Barbiett <146207003+Barbiett@users.noreply.github.com>
2024-03-18 18:28:48 +0200

    Таймер почти сделан.

---
+++ src/js/1-timer.js
@@ -0,0 +1,99 @@
+
+
+// Инициализируйте flatpickr на элементе выбора даты с указанием параметров, включая метод onClose для
+// обработки выбранной даты.
+
+// Напишите функцию обработчик для метода onClose, в которой проверяйте выбранную дату.Если она находится в
+// прошлом, сделайте кнопку "Start" неактивной и покажите сообщение об ошибке с помощью iziToast.
+// Если дата корректна, активируйте кнопку "Start".
+
+// Создайте функцию для добавления ведущего нуля к числу, если оно состоит из одной цифры.
+
+// Напишите функцию для расчета оставшегося времени до выбранной даты на основе разницы между текущим временем
+//  и выбранной датой.
+
+// Создайте функцию для обновления интерфейса таймера с учетом оставшегося времени.
+
+// Назначьте обработчик события click на кнопку "Start", который будет запускать таймер.
+//  Внутри обработчика делайте кнопку неактивной, запускайте таймер и выводите оставшееся время на страницу.
+
+// Напишите логику для остановки таймера, когда оставшееся время достигнет нуля.
+
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const selector = document.querySelector("#datetime-picker");
+
+const dataStart = document.querySelector("[data-start]");
+
+let userSelectedDate = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    const currentDate = new Date();
+    if (userSelectedDate < currentDate) {
+      iziToast.warning({
+        color: "red",
+        message: "Please choose a date in the future.",
+        position: "topRight"
+      });
+      dataStart.setAttribute("disabled");
+    } else {
+      dataStart.removeAttribute("disabled");
+    }
+  }
+};
+flatpickr(selector, options);
+
+const timer = document.querySelector(".timer");
+
+dataStart.addEventListener("click", startTimer);
+
+function startTimer() {
+  dataStart.setAttribute("disabled", true);
+  selector.setAttribute("disabled", true);
+
+  const intervalId = setInterval(() => {
+    const timeLeft = userSelectedDate - new Date();
+    if (timeLeft <= 0) {
+      clearInterval(intervalId);
+      timer.textContent = "00:00:00:00";
+      return
+    }
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    const formattedTime = `${addZeroOnStart(days)}:${addZeroOnStart(hours)}:${addZeroOnStart(minutes)}:${addZeroOnStart(seconds)}`;
+    timer.textContent = formattedTime;
+  }, 1000);
+}
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addZeroOnStart(value) {
+  return value < 10 ? "0" + value : value;
+}
